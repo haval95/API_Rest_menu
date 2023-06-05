d409d1fe6eba55b809540986fb698f36ccde0680
haval95 <havalrezan22@gmail.com>
2023-06-05 23:39:52 +0300

    cart model registered

--- restaurantMenuApi/rest_menu/admin.py
+++ restaurantMenuApi/rest_menu/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Category, MenuItem, Order, OrderItem
+from .models import Category, MenuItem, Order, OrderItem, CartItem, Cart
 
 
 admin.site.register(Category)
 admin.site.register(MenuItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(CartItem)
+admin.site.register(Cart)
