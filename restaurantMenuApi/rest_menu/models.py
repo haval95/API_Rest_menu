9ef3c650e1067e97a84877b95d6218d586d94e9d
haval95 <havalrezan22@gmail.com>
2023-06-05 23:02:55 +0300

    FEAT: Menu Item model created

--- restaurantMenuApi/rest_menu/models.py
+++ restaurantMenuApi/rest_menu/models.py
@@ -7,3 +7,17 @@ class Category (models.Model):
     def __str__(self):
         return self.name
     
+    
+class MenuItem (models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    ingridiants = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images', null=True)
+    category = models.ForeignKey(
+        Category, related_name="category", on_delete=models.PROTECT, default=None
+    )
+    
+
+    def __str__(self) -> str:
+        return self.name
+
