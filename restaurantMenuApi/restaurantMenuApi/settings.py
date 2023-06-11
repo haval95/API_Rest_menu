4ea7e25b8ee64b7f157e011102295ca12fe97bf0
haval95 <havalrezan22@gmail.com>
2023-06-11 21:04:05 +0300

    djoser installed

--- restaurantMenuApi/restaurantMenuApi/settings.py
+++ restaurantMenuApi/restaurantMenuApi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     'rest_menu',
+   
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
